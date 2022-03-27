b74a9ed85df40801ad67b5f93ac3b9f5d9f4182e
hugosenari <hugosenari@gmail.com>
2022-03-27 17:58:10 -0300

    chore: re index assimp

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-assimp-master.ref   = "refs/heads/master";
   inputs.src-assimp-master.owner = "barcharcraz";
   inputs.src-assimp-master.repo  = "nim-assimp";
+  inputs.src-assimp-master.dir   = "";
   inputs.src-assimp-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
